fe770573029ffc8f91883ff61bc5ad0c7fc74d51
Jaskamal Kainth <kainthjaskamal@gmail.com>
2015-12-31 01:47:38 +0530

    Greedy , Sorting , Structs , Bool Operator()

---
+++ IMBOX.cpp
@@ -0,0 +1,136 @@
+/*
+ *
+ *    J1K7_7
+ * 
+ */
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <cstring>
+#include <list>
+#include <map>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <limits>
+using namespace std;
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef long double        ld;
+typedef pair<int,int>      pii;
+typedef pair<ll,ll>        pll;
+typedef vector<int>        vi;
+typedef vector<long long>  vll;
+#define l(x) 		       (x << 1) + 1
+#define r(x) 		       (x << 1) + 2
+#define mid(l, r) 	       ((l + r) >> 1)
+#define mp                 make_pair
+#define pb                 push_back
+#define all(a)             a.begin(),a.end()
+#define pr(n)              printf("%d",n)
+#define s(n)               scanf("%d",&n)
+#define debug(x)	       {cerr <<#x<<" = " <<x<<"\n"; }
+#define debug2(x, y)       {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<"\n";}
+#define ss                 second
+#define ff                 first
+#define m0(x) 		       memset(x,0,sizeof(x))
+#define snuke(c,itr)       for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
+const int mod=1e9+7;
+
+const ll  mx_ll   = numeric_limits<ll> :: max();
+const int mx_int  = numeric_limits<int> :: max();
+
+const long double PI = (long double)(3.1415926535897932384626433832795);
+
+inline bool ispow2(int x){return (x!=0 && (x&(x-1))==0);} //0 or 1
+
+int msb(unsigned x){ union { double a; int b[2]; }; a = x; return (b[1] >> 20) - 1023; }
+
+template<class T>
+inline void cinarr(T a, int n){ for (int i=0;i<n;++i) cin >> a[i];}
+
+inline ll powmod(ll a,ll b) {ll res = 1; while(b){if(b&1) res = (res*a)%mod;a = (a*a)%mod;b >>= 1;}return res;}
+
+inline ll gcd(ll a,ll b){ll t;while(b){a=a%b;t=a;a=b;b=t;}return a;}
+inline ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+
+inline int nextint(){ int x; scanf("%d",&x); return x; }
+inline ll nextll(){ ll x; scanf("%lld",&x); return x; }
+
+struct rectangle
+{
+	int x1,y1;  
+	int x2,y2;  
+	ll area()
+	{
+		return 1ll*(x2-x1)*(y2-y1);  
+	}
+
+};
+bool operator < (const rectangle &r1 , const rectangle &r2)
+{
+	if ( r1.x1 == r2.x1 ) 
+		return r1.y1 < r2.y1;
+	return r1.x1 < r2.x1; 
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0);
+
+	int n; cin >> n ;
+
+	vector<rectangle> r;
+	r.resize(n);
+	for(int i = 0; i < n; i++)
+	{
+		cin >> r[i].x1 >> r[i].y1;  
+		cin >> r[i].x2 >> r[i].y2;  
+
+		if ( r[i].x1 > r[i].x2 ) 
+		{
+			swap ( r[i].x1 , r[i].x2);
+			swap ( r[i].y1 , r[i].y2);
+		}
+	}
+	sort(r.begin() , r.end() );
+
+	ll cur_max_x =  numeric_limits<ll> :: min();
+	ll cur_max_y =  numeric_limits<ll> :: min();
+
+	ll ans = 0ll;
+
+	for(int i = 0;  i < n ; i++)
+	{
+		if ( r[i].x1 < cur_max_x && r[i].y1 < cur_max_y ) {}
+		else
+		{
+			ans += r[i].area();  
+		}
+		// update the cur_max_x & cur_max_y
+		if ( r[i].x2 > cur_max_x || ( r[i].x2 == cur_max_x  && r[i].y2 > cur_max_y  ) ) 
+		{
+			cur_max_x = r[i].x2;  
+			cur_max_y = r[i].y2;
+		}
+	}
+
+
+	cout << ans << "\n";
+
+    return 0;
+}
+
